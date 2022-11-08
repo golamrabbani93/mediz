d57288c19dd857678f2d646ff059dffce69bf9d5
golamrabbani93 <golamrabbani9373@gmail.com>
2022-11-08 10:41:30 +0600

    load all services for services page and make breadcumb

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const Services = () => {
+	const [services, setServices] = useState([]);
+	console.log('🚀🚀: Services -> services', services);
+	useEffect(() => {
+		const url = `http://localhost:5000/services`;
+		fetch(url)
+			.then((res) => res.json())
+			.then((data) => {
+				setServices(data);
+			});
+	}, []);
 	return (
 		<div>
-			<h2>This is services page</h2>
+			<div className="breadcum bg-base-300 h-80 w-full flex justify-center items-center">
+				<h2 className="text-8xl font-bold text-white">Services</h2>
+			</div>
 		</div>
 	);
 };
