4eb51f2e6508f3fd147cd76d424f3c688bf849e2
golamrabbani93 <golamrabbani9373@gmail.com>
2022-11-08 10:45:05 +0600

    services page complete to show all services

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import SingleService from '../Home/Services/SingleService/SingleService';
 
 const Services = () => {
 	const [services, setServices] = useState([]);
@@ -15,6 +16,16 @@ const Services = () => {
 		<div>
 			<div className="breadcum bg-base-300 h-80 w-full flex justify-center items-center">
 				<h2 className="text-8xl font-bold text-white">Services</h2>
+			</div>
+			<div className="container mx-auto my-20 ">
+				<h5 className="text-4xl font-bold text-white text-center">
+					Total Services {services.length}
+				</h5>
+				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-10 gap-5">
+					{services.map((service) => (
+						<SingleService key={service._id} service={service}></SingleService>
+					))}
+				</div>
 			</div>
 		</div>
 	);
