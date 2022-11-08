f2bfd1ec3df39cdd00cfaf707137b4dd075ff6f0
golamrabbani93 <golamrabbani9373@gmail.com>
2022-11-08 21:31:29 +0600

    create user with email and password and set env file

--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -1,9 +1,16 @@
 import React, {createContext} from 'react';
-
+import {createUserWithEmailAndPassword, getAuth} from 'firebase/auth';
+import app from '../../firebase/firebase.config';
 export const AuthContext = createContext();
 const AuthProvider = ({children}) => {
 	const user = 'rabbani';
-	const authInfo = {user};
+	const auth = getAuth(app);
+
+	//*create user with email and password
+	const userCreateWithEmail = (email, password) => {
+		return createUserWithEmailAndPassword(auth, email, password);
+	};
+	const authInfo = {user, userCreateWithEmail};
 	return <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>;
 };
 
--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import {initializeApp} from 'firebase/app';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: 'AIzaSyC8wOyvsONiol71hxAmRrUkCmXL1SX_AlE',
-	authDomain: 'mediz-clinic.firebaseapp.com',
-	projectId: 'mediz-clinic',
-	storageBucket: 'mediz-clinic.appspot.com',
-	messagingSenderId: '553118800598',
-	appId: '1:553118800598:web:b8c417345f954ca330c777',
+	apiKey: process.env.REACT_APP_apiKey,
+	authDomain: process.env.REACT_APP_authDomain,
+	projectId: process.env.REACT_APP_projectId,
+	storageBucket: process.env.REACT_APP_storageBucket,
+	messagingSenderId: process.env.REACT_APP_messagingSenderId,
+	appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
