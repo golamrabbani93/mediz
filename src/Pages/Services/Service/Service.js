26f724fe064f82d06cbabe66d47403b9dcd7b4dd
golamrabbani93 <golamrabbani9373@gmail.com>
2022-11-08 20:50:15 +0600

    review section style complete

--- src/Pages/Services/Service/Service.js
+++ src/Pages/Services/Service/Service.js
@@ -2,10 +2,11 @@ import React from 'react';
 import {useLoaderData} from 'react-router-dom';
 import {PhotoProvider, PhotoView} from 'react-photo-view';
 import 'react-photo-view/dist/react-photo-view.css';
+import {FaQuoteRight} from 'react-icons/fa';
 const Service = () => {
 	const service = useLoaderData();
 	console.log('🚀🚀: Service -> service', service);
-	const {id, img, title, price, description} = service;
+	const {_id, img, title, price, description} = service;
 	return (
 		<div>
 			{/* //*service details section */}
@@ -24,6 +25,33 @@ const Service = () => {
 							</PhotoView>
 						</PhotoProvider>
 						<button className="btn btn-primary">Get Started</button>
+					</div>
+				</div>
+			</div>
+
+			{/* //*testimonial section */}
+			<div className="testimonial container mx-auto my-20">
+				<div className="testimonial-text text-center">
+					<h2 className="text-4xl font-bold text-white">Testimonial</h2>
+					<p className="text-6xl mt-5 text-warning font-bold ">
+						<FaQuoteRight className="mx-auto" />
+					</p>
+					<button className="btn btn-primary mt-7">Add Review</button>
+				</div>
+
+				<div className="testimonial mt-7">
+					<div className="single-testimonial flex border w-3/4 mx-auto justify-center items-center rounded-xl border-warning">
+						<div className="text p-10 text-white">
+							<p className="w-3/4 font-bold">
+								Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi explicabo placeat
+								iste tempore cum quo eligendi, beatae nostrum dolore nihil?
+							</p>
+							<h2 className="text-4xl text-warning font-bold">name</h2>
+							<h5 className="text-xl text-warning font-bold">date</h5>
+						</div>
+						<div className="img p-10 ">
+							<img className="rounded-full" src="https://placeimg.com/200/200/people" alt="" />
+						</div>
 					</div>
 				</div>
 			</div>
