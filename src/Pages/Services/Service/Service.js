5d0f3e007711cb6112a3e2f1aef2c5f3f050fcc1
golamrabbani93 <golamrabbani9373@gmail.com>
2022-11-08 20:17:01 +0600

    service details section complete

--- src/Pages/Services/Service/Service.js
+++ src/Pages/Services/Service/Service.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import {useLoaderData} from 'react-router-dom';
-
+import {PhotoProvider, PhotoView} from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
 const Service = () => {
 	const service = useLoaderData();
 	console.log('🚀🚀: Service -> service', service);
+	const {id, img, title, price, description} = service;
 	return (
 		<div>
-			<h2>this is services detail page</h2>
+			{/* //*service details section */}
+			<div className="hero h-screen bg-base-200">
+				<div className="hero-content text-center">
+					<div className=" text-white">
+						<h1 className="text-5xl font-bold">{title}</h1>
+						<p className="py-6">
+							Price:
+							<span className="text-5xl font-bold  text-yellow-500">${price}</span>
+						</p>
+						<p className="pb-6 w-3/4 mx-auto">{description}</p>
+						<PhotoProvider>
+							<PhotoView src={img}>
+								<img className="p-4 rounded-3xl cursor-pointer mx-auto" src={img} alt="Shoes" />
+							</PhotoView>
+						</PhotoProvider>
+						<button className="btn btn-primary">Get Started</button>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
