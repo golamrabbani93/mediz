c2f05b1b2a89ec881a68a0199158f3496ebf4064
golamrabbani93 <golamrabbani9373@gmail.com>
2022-11-09 21:34:56 +0600

    implement react spinner at private router

--- src/Routes/PrivateRoutes/PrivateRoutes.js
+++ src/Routes/PrivateRoutes/PrivateRoutes.js
@@ -1,12 +1,16 @@
 import React, {useContext} from 'react';
 import {Navigate, useLocation} from 'react-router-dom';
 import {AuthContext} from '../../contexts/AuthProvider/AuthProvider';
-
+import {ScaleLoader} from 'react-spinners';
 const PrivateRoutes = ({children}) => {
 	const {user, loader} = useContext(AuthContext);
 	const location = useLocation();
 	if (loader) {
-		return <h2>Loading....</h2>;
+		return (
+			<div className="flex justify-center items-center h-96">
+				<ScaleLoader color="#F26651"></ScaleLoader>
+			</div>
+		);
 	}
 	if (!user) {
 		return <Navigate to="/signin" state={{from: location}} replace />;
