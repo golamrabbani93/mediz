09cd35cce2e921e04b55b5afc1ff87bd9f47d111
golamrabbani93 <golamrabbani9373@gmail.com>
2022-11-09 11:36:11 +0600

    shoow single review to ui

--- src/Pages/Services/Review/Review.js
+++ src/Pages/Services/Review/Review.js
@@ -1,18 +1,17 @@
 import React from 'react';
 
-const Review = () => {
+const Review = ({review}) => {
+	console.log('🚀🚀: Review -> review', review);
+	const {name, message, email, img} = review;
 	return (
-		<div className="single-testimonial flex border w-3/4 mx-auto justify-center items-center rounded-xl border-warning mt-5">
+		<div className="single-testimonial flex border w-3/4 mx-auto justify-between items-center rounded-xl border-warning mt-5">
 			<div className="text p-10 text-white">
-				<p className="w-3/4 font-bold">
-					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi explicabo placeat iste
-					tempore cum quo eligendi, beatae nostrum dolore nihil?
-				</p>
-				<h2 className="text-4xl text-warning font-bold">name</h2>
-				<h5 className="text-xl text-warning font-bold">date</h5>
+				<p className="w-3/4 font-bold">{message}</p>
+				<h2 className="text-4xl text-warning font-bold">{name}</h2>
+				<h5 className="text-xl text-warning font-bold">{email}</h5>
 			</div>
 			<div className="img p-10 ">
-				<img className="rounded-full" src="https://placeimg.com/200/200/people" alt="" />
+				<img className="rounded-full" style={{width: '100px', height: '100px'}} src={img} alt="" />
 			</div>
 		</div>
 	);
