f14e0ef3ce94f3e34d0e2f4cedbdc5668a41869e
golamrabbani93 <golamrabbani9373@gmail.com>
2022-11-10 11:34:55 +0600

    add service complete

--- src/Pages/AddServices/AddServices.js
+++ src/Pages/AddServices/AddServices.js
@@ -1,9 +1,74 @@
 import React from 'react';
+import toast from 'react-hot-toast';
 
 const AddServices = () => {
+	const addServices = (e) => {
+		e.preventDefault();
+		const form = e.target;
+		const description = form.description.value;
+		const title = form.title.value;
+		const price = form.price.value;
+		const photo = form.photo.value;
+		console.log(description, title, price, photo);
+		const service = {
+			title: title,
+			img: photo,
+			price: price,
+			description: description,
+		};
+		fetch(`http://localhost:5000/services/`, {
+			method: 'POST',
+			headers: {
+				'content-type': 'application/json',
+			},
+			body: JSON.stringify(service),
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				if (data.acknowledged) {
+					toast.success('Add Service Successful');
+					form.reset();
+				}
+			});
+	};
 	return (
-		<div>
-			<h2>add service</h2>
+		<div className="container mx-auto text-white text-center mt-7">
+			<h2 className="text-5xl mt-10 mb-8">Update </h2>
+			<form onSubmit={addServices}>
+				<div className="">
+					<textarea
+						className="textarea textarea-secondary"
+						style={{width: '280px'}}
+						placeholder="Description"
+						name="description"
+					></textarea>
+				</div>
+				<div className="text-center m-4 md:m-0">
+					<input
+						type="text"
+						placeholder="Title"
+						name="title"
+						className="mt-5 input input-bordered input-secondary w-full max-w-xs"
+					/>
+					<br />
+
+					<input
+						type="text"
+						placeholder="Photo URL"
+						name="photo"
+						className="  mt-5 input input-bordered input-secondary w-full max-w-xs"
+					/>
+					<input
+						type="text"
+						placeholder="Price"
+						name="price"
+						className="ml-5  mt-5 input input-bordered input-secondary w-full max-w-xs"
+					/>
+				</div>
+				<button className="btn btn-secondary mt-5" type="submit">
+					Add Service
+				</button>
+			</form>
 		</div>
 	);
 };
